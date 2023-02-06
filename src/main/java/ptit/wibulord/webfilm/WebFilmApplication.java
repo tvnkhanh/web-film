f39576e2a2cf69e45ea208713648a3b0e9e759d3
tvnkhanh <tuvankhanh2002@gmail.com>
2023-02-06 20:12:28 +0700

    first commit

---
+++ src/main/java/ptit/wibulord/webfilm/WebFilmApplication.java
@@ -0,0 +1,13 @@
+package ptit.wibulord.webfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFilmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFilmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ptit/wibulord/webfilm/WebFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package ptit.wibulord.webfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
